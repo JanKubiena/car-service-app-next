5a97c39dcb7d1f861bb3b33e5e8aeb97053b158c
Jan Kubiena <jan@kubiena.pl>
2025-04-23 15:38:16 +0200

    connected project with firebase and setup authentication

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
+'use client';
+
 import Image from "next/image";
 
+import { useAuth } from './context/AuthContext';
+import Login from './login/Login';
+
 export default function Home() {
+  const { user, logOut } = useAuth();
+
+  if (!user) {
+    return <Login />;
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <button onClick={logOut}>Sign out</button>
         <Image
           className="dark:invert"
           src="/next.svg"
