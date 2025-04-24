59d8d5defa0250b41fb5941579073b27b630ca21
Jan Kubiena <jan@kubiena.pl>
2025-04-24 21:17:38 +0200

    confix fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // use 'standalone' if you're using SSR
-    trailingSlash: true,
-    images: {
-      unoptimized: true, // especially for static exports
-    },
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
