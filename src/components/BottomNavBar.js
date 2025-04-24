2e72114f02ff470a1d16b40017c4ff2bd67d8f27
Jan Kubiena <jan@kubiena.pl>
2025-04-24 12:13:32 +0200

    add bottom navigation bar + delated all global styles

---
+++ src/components/BottomNavBar.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function BottomNavBar() {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 bg-white text-black shadow-md flex justify-around items-center h-16">
+      <Link href="/" className="flex flex-col items-center">
+          <Image src="/icons/home.png" alt="Home" width={24} height={24} />
+          <span className="text-xs">Home</span>
+      </Link>
+      <Link href="/" className="flex flex-col items-center">
+          <Image src="/icons/order.png" alt="Ord" width={24} height={24} />
+          <span className="text-xs">Orders</span>
+      </Link>
+      <Link href="/" className="flex flex-col items-center">
+          <Image src="/icons/visits.png" alt="Vis" width={24} height={24} />
+          <span className="text-xs">Visits</span>
+      </Link>
+      <Link href="/" className="flex flex-col items-center">
+          <Image src="/icons/user.png" alt="Acc" width={24} height={24} />
+          <span className="text-xs">Account</span>
+      </Link>
+    </nav>
+  );
+}
